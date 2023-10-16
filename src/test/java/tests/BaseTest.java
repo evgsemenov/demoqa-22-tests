e1c214c2595129d46e23b7788e3cd227dd708151
Evgenii Semenov <eo.semenoff@gmail.com>
2023-10-16 22:24:28 +0300

    basetest modified

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,9 +1,12 @@
 package tests;
 
+import com.codeborne.selenide.WebDriverRunner;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import pages.RegistrationPage;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -12,36 +15,41 @@ import org.junit.jupiter.api.BeforeEach;
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWindow;
+import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.impl.Cleanup.of;
 
 public class BaseTest {
     RegistrationPage registrationPage = new RegistrationPage();
 
-//    @BeforeAll
+//    @BeforeEach
 //    void openPage(){
-//        registrationPage.openPage();
-//    }
+//    registrationPage.openPage();
+//}
+
+    @BeforeEach
+    void  addAllureListener(){
+    SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+}
 
     @BeforeAll
     static void beforeAll() {
-        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        open("https://demoqa.com/automation-practice-form");
         DesiredCapabilities capabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities = capabilities;
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-        Configuration.browserCapabilities = capabilities;
-        Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = "1920x1080";
-        Configuration.baseUrl = "https://demoqa.com/";
-        Configuration.timeout = 10000;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+//        Configuration.baseUrl = "https://demoqa.com/";
+
     }
 
-    @BeforeEach
-    void openPage(){
-        registrationPage.openPage();
-    }
+
 
     @AfterEach
     void addAttachments() {
@@ -55,5 +63,5 @@ public class BaseTest {
     static void afterAll(){
         closeWindow();
     }
-    }
+}
 
